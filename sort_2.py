5398655b4a9e00a1ee012a13cfb37611529af508
Markusbra <151835238+Markusbra@users.noreply.github.com>
2023-11-23 11:56:16 +0100

    Add files via upload

---
+++ sort_2.py
@@ -0,0 +1,13 @@
+def sort_2(arr):
+    """Quicksort"""
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    return sort_2(left) + middle + sort_2(right)
+
+
